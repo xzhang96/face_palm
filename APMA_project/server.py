49eca3135ba0b2403dd8255f2a0c9cf283326ef2
xzhang96 <xz2582@columbia.edu>
2017-04-27 06:43:04 +0000

    edited server.py

--- APMA_project/server.py
+++ APMA_project/server.py
@@ -35,5 +35,34 @@ def show():
     return send_from_directory(os.path.join(APP_ROOT, 'static/'), filename='img.jpg', as_attachment=True)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using:
+
+        python server.py
+
+    Show the help text using:
+
+        python server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    print( "running on %s:%d" % (HOST, PORT))
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+  run()
+
